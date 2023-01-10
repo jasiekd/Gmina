b52c070aac018cc8cb386d3ec980678a55e30ce3
Makrol <karolgardian6@gmail.com>
2023-01-10 19:54:31 +0100

    Poprawki

--- Gmina/Body/AddUserBody.cs
+++ Gmina/Body/AddUserBody.cs
@@ -16,10 +16,55 @@ namespace Gmina.Body
         {
             InitializeComponent();
         }
-
+        void clearForm()
+        {
+            NameTextBox.Clear();
+            SurnameTextBox.Clear();
+            BirthdateTextBox.ResetText();
+            EmailTextBox.Clear();
+            LoginTextBox.Clear();
+            PasswordTextBox.Clear();
+            PhoneNumberTextBox.Clear();
+            HouseNumberTextBox.Clear();
+            StreetTextBox.Clear();
+            ZipCodeTextBox.Clear();
+            PESELTextBox.Clear();
+        }
         private void AddUser_Click(object sender, EventArgs e)
         {
+            /* Sposob pobierania danych z formulaza
+            string tmp;
+            tmp = NameTextBox.Text;
+            tmp = SurnameTextBox.Text;
+            tmp = BirthdateTextBox.Text;
+            tmp = EmailTextBox.Text;
+            tmp = LoginTextBox.Text;
+            tmp = PasswordTextBox.Text;
+            tmp = PhoneNumberTextBox.Text;
+            tmp = HouseNumberTextBox.Text;
+            tmp = StreetTextBox.Text;
+            tmp = ZipCodeTextBox.Text;
+            tmp = PESELTextBox.Text;
+            */
 
+            // To do: doanie usera do bazy
+            if (true)//jeżeli uda się dodać użytkownika
+            {
+                DialogResult result = MessageBox.Show("Użytkownik dodany poprawnie", "Dodawanie użytkownika", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    clearForm();
+                }
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("Problem z dodanie użytkownika", "Dodawanie użytkownika", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (result == DialogResult.OK)
+                {
+                    
+                }
+            }
+            
         }
     }
 }
