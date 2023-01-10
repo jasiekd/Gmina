71ec27289da81ccc496b459e5aeebe15db801061
Makrol <karolgardian6@gmail.com>
2023-01-10 19:28:10 +0100

    Rozbudowa i modernizacja oddawania głosów

--- Gmina/Body/ElectionPage.Designer.cs
+++ Gmina/Body/ElectionPage.Designer.cs
@@ -28,86 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.CandidatesList = new System.Windows.Forms.DataGridView();
-            this.inhabitantID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.inhabitantName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.inhabitantSurname = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.inhabitantBirthday = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.vote = new System.Windows.Forms.DataGridViewButtonColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.CandidatesList)).BeginInit();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // CandidatesList
-            // 
-            this.CandidatesList.AllowUserToAddRows = false;
-            this.CandidatesList.AllowUserToDeleteRows = false;
-            this.CandidatesList.AllowUserToResizeRows = false;
-            this.CandidatesList.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(59)))), ((int)(((byte)(128)))));
-            this.CandidatesList.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.CandidatesList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.CandidatesList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.inhabitantID,
-            this.inhabitantName,
-            this.inhabitantSurname,
-            this.inhabitantBirthday,
-            this.vote});
-            this.CandidatesList.Location = new System.Drawing.Point(3, 3);
-            this.CandidatesList.MinimumSize = new System.Drawing.Size(788, 450);
-            this.CandidatesList.Name = "CandidatesList";
-            this.CandidatesList.ReadOnly = true;
-            this.CandidatesList.RowHeadersVisible = false;
-            this.CandidatesList.RowHeadersWidth = 51;
-            this.CandidatesList.RowTemplate.Height = 25;
-            this.CandidatesList.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
-            this.CandidatesList.Size = new System.Drawing.Size(835, 450);
-            this.CandidatesList.TabIndex = 7;
-            this.CandidatesList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.CandidatesList_CellContentClick);
-            // 
-            // inhabitantID
-            // 
-            this.inhabitantID.HeaderText = "ID";
-            this.inhabitantID.MinimumWidth = 6;
-            this.inhabitantID.Name = "inhabitantID";
-            this.inhabitantID.ReadOnly = true;
-            this.inhabitantID.Width = 50;
-            // 
-            // inhabitantName
-            // 
-            this.inhabitantName.HeaderText = "Imie";
-            this.inhabitantName.MinimumWidth = 6;
-            this.inhabitantName.Name = "inhabitantName";
-            this.inhabitantName.ReadOnly = true;
-            this.inhabitantName.Width = 250;
-            // 
-            // inhabitantSurname
-            // 
-            this.inhabitantSurname.HeaderText = "Nazwisko";
-            this.inhabitantSurname.MinimumWidth = 6;
-            this.inhabitantSurname.Name = "inhabitantSurname";
-            this.inhabitantSurname.ReadOnly = true;
-            this.inhabitantSurname.Width = 143;
-            // 
-            // inhabitantBirthday
-            // 
-            this.inhabitantBirthday.HeaderText = "Data urodzenia";
-            this.inhabitantBirthday.MinimumWidth = 6;
-            this.inhabitantBirthday.Name = "inhabitantBirthday";
-            this.inhabitantBirthday.ReadOnly = true;
-            this.inhabitantBirthday.Width = 200;
-            // 
-            // vote
-            // 
-            this.vote.HeaderText = "Zagłosuj";
-            this.vote.MinimumWidth = 6;
-            this.vote.Name = "vote";
-            this.vote.ReadOnly = true;
-            this.vote.Text = "Zagłosuj";
-            this.vote.ToolTipText = "Zagłosuj";
-            this.vote.UseColumnTextForButtonValue = true;
-            this.vote.Width = 80;
             // 
             // label1
             // 
@@ -123,7 +46,6 @@
             // panel1
             // 
             this.panel1.AutoScroll = true;
-            this.panel1.Controls.Add(this.CandidatesList);
             this.panel1.Location = new System.Drawing.Point(0, 105);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(860, 413);
@@ -141,8 +63,6 @@
             this.Name = "ElectionPage";
             this.Text = "ElectionPage";
             this.Load += new System.EventHandler(this.ElectionPage_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.CandidatesList)).EndInit();
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -150,6 +70,7 @@
 
         #endregion
         private List<ButtonRounded> optionButtonList;
+        private List<Label> optionLabelList;
         private DataGridView CandidatesList;
         private DataGridViewTextBoxColumn inhabitantID;
         private DataGridViewTextBoxColumn inhabitantName;
--- Gmina/Body/ElectionPage.cs
+++ Gmina/Body/ElectionPage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,11 +21,74 @@ namespace Gmina.Body
 
         private void ElectionPage_Load(object sender, EventArgs e)
         {
-            this.panel1.Controls.Add(new ButtonRounded());
+            createOptionList();
+        }
+        private void doVote(object sender, EventArgs e)
+        {
+            ButtonRounded buttonSender = (ButtonRounded)sender;
+            Debug.WriteLine("constructor fired"+buttonSender.TabIndex);// odczyt przekazanego parametru
+
+            //To do: wysłanie oddanego głosu na serwer
+
+            DialogResult result = MessageBox.Show("Czy jesteś pewny/pewna swojego wyboru?", "Głosowanie", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+        private void createOptionList()
+        {
+            //To do: należy odczytać z serwera odpowiednie informacje o głosowaniu i według wskazówek załadować je w pętli do wyświetlenia 
+            ButtonRounded tmp;
+            Label tmpLabel;
+            Panel tmpPanel;
+            this.optionButtonList = new List<ButtonRounded>();
+            this.optionLabelList = new List<Label>();
+            for (int i=0;i<30;i++)
+            {
+                tmp = new ButtonRounded();
+                tmp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(64)))), ((int)(((byte)(148)))));
+                tmp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+                tmp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+                tmp.Location = new System.Drawing.Point(500,0);
+                tmp.Name = "VoteButton"+i;
+                tmp.Size = new System.Drawing.Size(219, 50);
+                tmp.Text = "Zagłosuj"+i;
+                tmp.TabIndex = i; //To do: w tabindex można przekazać liczbe do funkcji doVote np jakieś id do czegoś na serwerze
+                tmp.Click += new System.EventHandler(this.doVote);
+                this.optionButtonList.Add(tmp);
+
+                tmpLabel = new Label();
+                tmpLabel.AutoSize = true;
+                tmpLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+                tmpLabel.ForeColor = System.Drawing.Color.White;
+                tmpLabel.Location = new System.Drawing.Point(0, 0);
+                tmpLabel.Name = "Votelabel"+i;
+                tmpLabel.Size = new System.Drawing.Size(191, 37);
+                tmpLabel.Text = "To do: Opis opcji glosowania pobranej z bazy z bazy"+i; //To do: załadowanie z bazy opisu
+
+
+                this.optionLabelList.Add(tmpLabel);
+                tmpPanel=new Panel();
+                if (i % 2 == 0)
+                {
+                    tmpPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(95)))), ((int)(((byte)(207)))));
+                }
+                else{
+                    tmpPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(90)))), ((int)(((byte)(245)))));
+                }
+                
+                tmpPanel.Location = new System.Drawing.Point(0, 50 * i);
+                tmpPanel.Name = "VotePanel"+i;
+                tmpPanel.Size = new System.Drawing.Size(860, 50);
+                tmpPanel.Controls.Add(tmp);
+                tmpPanel.Controls.Add(tmpLabel);
+
+                this.panel1.Controls.Add(tmpPanel);
+            }
+
+           
             
-            CandidatesList.Rows.Clear();
-            CandidatesList.Rows.Add(1, "t", "t", DateTime.Now);
-            CandidatesList.Rows.Add(1, "a", "a", DateTime.Now);
         }
         public void Election(int electionID)
         {
