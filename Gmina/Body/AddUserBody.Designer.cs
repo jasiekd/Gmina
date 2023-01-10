60a9d052ad82d23c4fbc009f78ea59b8b619a601
Damian Gajda <damian.gajda-2000@wp.pl>
2023-01-10 19:26:55 +0100

    Dodanie strony do dodawania nowych użytkowników

---
+++ Gmina/Body/AddUserBody.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace Gmina.Body
+{
+    partial class AddUserBody
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SurnameTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.PhoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.HouseNumberTextBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.StreetTextBox = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.ZipCodeTextBox = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.PESELTextBox = new System.Windows.Forms.TextBox();
+            this.AddUser = new Gmina.ButtonRounded();
+            this.BirthdateTextBox = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(246, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(398, 46);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dodanie użytkownika";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(49, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Imię:";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(256, 106);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(263, 27);
+            this.NameTextBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(49, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nazwisko:";
+            // 
+            // SurnameTextBox
+            // 
+            this.SurnameTextBox.Location = new System.Drawing.Point(256, 156);
+            this.SurnameTextBox.Name = "SurnameTextBox";
+            this.SurnameTextBox.Size = new System.Drawing.Size(263, 27);
+            this.SurnameTextBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(49, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Data urodzenia:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(49, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Email:";
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Location = new System.Drawing.Point(256, 260);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(263, 27);
+            this.EmailTextBox.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(49, 315);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Login:";
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Location = new System.Drawing.Point(256, 308);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(263, 27);
+            this.LoginTextBox.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(49, 363);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Hasło:";
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(256, 356);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(263, 27);
+            this.PasswordTextBox.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(49, 411);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(128, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Numer telefonu:";
+            // 
+            // PhoneNumberTextBox
+            // 
+            this.PhoneNumberTextBox.Location = new System.Drawing.Point(256, 404);
+            this.PhoneNumberTextBox.Name = "PhoneNumberTextBox";
+            this.PhoneNumberTextBox.Size = new System.Drawing.Size(263, 27);
+            this.PhoneNumberTextBox.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(49, 462);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(110, 20);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Numer domu:";
+            // 
+            // HouseNumberTextBox
+            // 
+            this.HouseNumberTextBox.Location = new System.Drawing.Point(256, 455);
+            this.HouseNumberTextBox.Name = "HouseNumberTextBox";
+            this.HouseNumberTextBox.Size = new System.Drawing.Size(263, 27);
+            this.HouseNumberTextBox.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(49, 509);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(52, 20);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Ulica:";
+            // 
+            // StreetTextBox
+            // 
+            this.StreetTextBox.Location = new System.Drawing.Point(256, 502);
+            this.StreetTextBox.Name = "StreetTextBox";
+            this.StreetTextBox.Size = new System.Drawing.Size(263, 27);
+            this.StreetTextBox.TabIndex = 21;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(49, 557);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(118, 20);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Kod pocztowy:";
+            // 
+            // ZipCodeTextBox
+            // 
+            this.ZipCodeTextBox.Location = new System.Drawing.Point(256, 550);
+            this.ZipCodeTextBox.Name = "ZipCodeTextBox";
+            this.ZipCodeTextBox.Size = new System.Drawing.Size(263, 27);
+            this.ZipCodeTextBox.TabIndex = 23;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label12.ForeColor = System.Drawing.Color.White;
+            this.label12.Location = new System.Drawing.Point(49, 609);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(68, 20);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "PESEL:";
+            // 
+            // PESELTextBox
+            // 
+            this.PESELTextBox.Location = new System.Drawing.Point(256, 602);
+            this.PESELTextBox.Name = "PESELTextBox";
+            this.PESELTextBox.Size = new System.Drawing.Size(263, 27);
+            this.PESELTextBox.TabIndex = 25;
+            // 
+            // AddUser
+            // 
+            this.AddUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(64)))), ((int)(((byte)(190)))));
+            this.AddUser.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AddUser.FlatAppearance.BorderSize = 0;
+            this.AddUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AddUser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            this.AddUser.Location = new System.Drawing.Point(376, 701);
+            this.AddUser.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.AddUser.Name = "AddUser";
+            this.AddUser.Size = new System.Drawing.Size(230, 91);
+            this.AddUser.TabIndex = 27;
+            this.AddUser.Text = "Dodaj";
+            this.AddUser.UseVisualStyleBackColor = false;
+            this.AddUser.Click += new System.EventHandler(this.AddUser_Click);
+            // 
+            // BirthdateTextBox
+            // 
+            this.BirthdateTextBox.Location = new System.Drawing.Point(256, 210);
+            this.BirthdateTextBox.Name = "BirthdateTextBox";
+            this.BirthdateTextBox.Size = new System.Drawing.Size(263, 27);
+            this.BirthdateTextBox.TabIndex = 28;
+            // 
+            // AddUserBody
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(59)))), ((int)(((byte)(128)))));
+            this.Controls.Add(this.BirthdateTextBox);
+            this.Controls.Add(this.AddUser);
+            this.Controls.Add(this.PESELTextBox);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.ZipCodeTextBox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.StreetTextBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.HouseNumberTextBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.PhoneNumberTextBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.LoginTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.SurnameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddUserBody";
+            this.Size = new System.Drawing.Size(901, 875);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox NameTextBox;
+        private Label label3;
+        private TextBox SurnameTextBox;
+        private Label label4;
+        private Label label5;
+        private TextBox EmailTextBox;
+        private Label label6;
+        private TextBox LoginTextBox;
+        private Label label7;
+        private TextBox PasswordTextBox;
+        private Label label8;
+        private TextBox PhoneNumberTextBox;
+        private Label label9;
+        private TextBox HouseNumberTextBox;
+        private Label label10;
+        private TextBox StreetTextBox;
+        private Label label11;
+        private TextBox ZipCodeTextBox;
+        private Label label12;
+        private TextBox PESELTextBox;
+        private ButtonRounded AddUser;
+        private DateTimePicker BirthdateTextBox;
+    }
+}
