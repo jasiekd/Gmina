529ac1f8f1b11137166cb65a9616d404e5c0cd60
Jasiek Dyrduł <johnygaming2@gmail.com>
2023-01-24 19:30:23 +0100

    GminaTests

---
+++ GminaTests/Body/Plus500ApplicationTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gmina.Body;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gmina.Body.Tests
+{
+    [TestClass()]
+    public class Plus500ApplicationTests
+    {
+        [TestMethod()]
+        public void send500PlusApp_ClickTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
